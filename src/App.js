490fd83457f2857398f32eba3e725f7d7e831cbb
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-09-23 19:44:45 +0530

    App Routing Functionality done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
-import leftSideBar from "./Components/leftSideBar";
-
+import Home from "./Components/Home";
+import Browse from "./Components/Browse";
+import About from "./Components/About";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
-    <div>
-      {/* <leftSideBar /> */}
-      <Dashboard />
-    </div>
+    <>
+      <Router>
+        <Dashboard />
+        {/* <Switch>
+          <Route exact from="/" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/browse"
+            render={(props) => <Browse {...props} />}
+          />
+          <Route exact path="/about" render={(props) => <About {...props} />} />
+        </Switch> */}
+      </Router>
+    </>
   );
 };
 
