e8433f1bf30b20eae4c6cf6886b5c62e41c064ca
Harsh Dhorajia <harshdhorajiya1304@gmail.com>
2020-09-29 11:09:12 +0530

    Songlist and Player component added

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -13,6 +13,8 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import MailIcon from "@material-ui/icons/Mail";
 import MenuIcon from "@material-ui/icons/Menu";
+import MoreIcon from "@material-ui/icons/More";
+
 import SearchIcon from '@material-ui/icons/Search';
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -22,7 +24,8 @@ import About from "./About";
 import Home from "./Home";
 import Browse from "./Browse";
 import Profile from "./Profile";
-import { InputBase } from "@material-ui/core";
+import { Badge, Button, InputBase, Menu, MenuItem } from "@material-ui/core";
+import { AccountCircle } from "@material-ui/icons";
 
 const drawerWidth = 240;
 
@@ -107,7 +110,45 @@ function ResponsiveDrawer(props) {
   const handleDrawerToggle = () => {
     setMobileOpen(!mobileOpen);
   };
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
   const [state, setState] = React.useState("Home");
 
   const itemList = [
@@ -127,7 +168,7 @@ function ResponsiveDrawer(props) {
         history.push("/browse");
       },
     },
-    { 
+    {
       text: "About",
       icon: <MailIcon />,
       onClick: () => {
@@ -182,7 +223,7 @@ function ResponsiveDrawer(props) {
           >
             <MenuIcon />
           </IconButton>
-          <div className={classes.search} style={{borderRadius:"25px", marginLeft:"-5px"}}>
+          <div className={classes.search} style={{ borderRadius: "25px", marginLeft: "-5px" }}>
             <div className={classes.searchIcon}>
               <SearchIcon />
             </div>
@@ -193,11 +234,28 @@ function ResponsiveDrawer(props) {
                 root: classes.inputRoot,
                 input: classes.inputInput,
               }}
-              style={{width: "500px", borderRadius:"50%"}}
+              style={{ width: "500px", borderRadius: "50%" }}
             />
           </div>
+          <div style={{ display: "flex", justifyContent: "flex-end", width: "100%", padding: "0" }}>
+            {/* <Button color="inherit">Login</Button> */}
+            <Button
+              edge="end"
+              aria-label="account of current user"
+              aria-controls={menuId}
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <Typography>
+                Shubham Khunt
+              </Typography>
+            </Button>
+          </div>
+
         </Toolbar>
       </AppBar>
+      {renderMenu}
       <nav className={classes.drawer} aria-label="mailbox folders">
         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
         <Hidden smUp implementation="css">
@@ -234,36 +292,6 @@ function ResponsiveDrawer(props) {
         {state == "Home" ? <Home /> : null}
         {state == "About" ? <About /> : null}
         {state == "Browse" ? <Browse /> : null}
-
-        {/* <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-          ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-          elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-          sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-          mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-          risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-          purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-          tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-          morbi tristique senectus et. Adipiscing elit duis tristique
-          sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-          eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-          posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography> */}
       </main>
     </div>
   );
@@ -279,241 +307,3 @@ ResponsiveDrawer.propTypes = {
 
 export default withRouter(ResponsiveDrawer);
 
-// import React from "react";
-// import PropTypes from "prop-types";
-// import AppBar from "@material-ui/core/AppBar";
-// import CssBaseline from "@material-ui/core/CssBaseline";
-// import Divider from "@material-ui/core/Divider";
-// import Drawer from "@material-ui/core/Drawer";
-// import Hidden from "@material-ui/core/Hidden";
-// import IconButton from "@material-ui/core/IconButton";
-// import InboxIcon from "@material-ui/icons/MoveToInbox";
-// import List from "@material-ui/core/List";
-// import ListItem from "@material-ui/core/ListItem";
-// import ListItemIcon from "@material-ui/core/ListItemIcon";
-// import ListItemText from "@material-ui/core/ListItemText";
-// import MenuIcon from "@material-ui/icons/Menu";
-// import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-// import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
-// import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
-// import LibraryBooksOutlinedIcon from "@material-ui/icons/LibraryBooksOutlined";
-// import DescriptionOutlinedIcon from "@material-ui/icons/DescriptionOutlined";
-// import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
-// import VideocamOutlinedIcon from "@material-ui/icons/VideocamOutlined";
-// import InsertDriveFileOutlinedIcon from "@material-ui/icons/InsertDriveFileOutlined";
-// import Toolbar from "@material-ui/core/Toolbar";
-// import Typography from "@material-ui/core/Typography";
-// import { makeStyles, useTheme } from "@material-ui/core/styles";
-// import Home from "./Home";
-// import Browse from "./Browse";
-
-// const drawerWidth = 240;
-
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     display: "flex",
-//   },
-//   drawer: {
-//     [theme.breakpoints.up("sm")]: {
-//       width: drawerWidth,
-//       flexShrink: 0,
-//     },
-//   },
-//   appBar: {
-//     [theme.breakpoints.up("sm")]: {
-//       width: `calc(100% - ${drawerWidth}px)`,
-//       marginLeft: drawerWidth,
-//     },
-//   },
-//   menuButton: {
-//     marginRight: theme.spacing(2),
-//     [theme.breakpoints.up("sm")]: {
-//       display: "none",
-//     },
-//   },
-//   // necessary for content to be below app bar
-//   toolbar: theme.mixins.toolbar,
-//   drawerPaper: {
-//     width: drawerWidth,
-//   },
-//   content: {
-//     flexGrow: 1,
-//     padding: theme.spacing(3),
-//   },
-// }));
-
-// function ResponsiveDrawer(props) {
-//   const { window } = props;
-//   const classes = useStyles();
-//   const theme = useTheme();
-//   const [mobileOpen, setMobileOpen] = React.useState(false);
-
-//   const handleDrawerToggle = () => {
-//     setMobileOpen(!mobileOpen);
-//   };
-
-//   const drawer = (
-//     <Router>
-//       <div>
-//         <div className={classes.toolbar} />
-//         <Divider />
-//         <List>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <HomeOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <Link to="/home">Home</Link>
-//           </ListItem>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <HomeOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <Link to="/browse">Browse</Link>
-//           </ListItem>
-//           {/* <ListItem button>
-//             <ListItemIcon>
-//               <SearchOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <Link to="/browse">Browse</Link>
-//             <ListItemText primary={"Browse"} />
-//           </ListItem> */}
-//           <ListItem button>
-//             <ListItemIcon>
-//               <DescriptionOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <ListItemText primary={"Album"} />
-//           </ListItem>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <PersonOutlineOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <ListItemText primary={"Artists"} />
-//           </ListItem>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <VideocamOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <ListItemText primary={"Video"} />
-//           </ListItem>
-//         </List>
-//         <Divider />
-//         <List>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <LibraryBooksOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <ListItemText primary={"Recent Played"} />
-//           </ListItem>
-//           <ListItem button>
-//             <ListItemIcon>
-//               <InsertDriveFileOutlinedIcon fontSize="large" cursor="pointer" />
-//             </ListItemIcon>
-//             <ListItemText primary={"Local Files"} />
-//           </ListItem>
-//         </List>
-//         <Route path="/home" exact component={Home} />
-//         <Route path="/browser" component={Browse} />
-//       </div>
-//     </Router>
-//   );
-
-//   const container =
-//     window !== undefined ? () => window().document.body : undefined;
-
-//   return (
-//     <div className={classes.root}>
-//       <CssBaseline />
-//       <AppBar position="fixed" className={classes.appBar}>
-//         <Toolbar>
-//           <IconButton
-//             color="inherit"
-//             aria-label="open drawer"
-//             edge="start"
-//             onClick={handleDrawerToggle}
-//             className={classes.menuButton}
-//           >
-//             <MenuIcon />
-//           </IconButton>
-//           <Typography variant="h6" noWrap>
-//             Responsive drawer
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//       <nav className={classes.drawer} aria-label="mailbox folders">
-//         {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-//         <Hidden smUp implementation="css">
-//           <Drawer
-//             container={container}
-//             variant="temporary"
-//             anchor={theme.direction === "rtl" ? "right" : "left"}
-//             open={mobileOpen}
-//             onClose={handleDrawerToggle}
-//             classes={{
-//               paper: classes.drawerPaper,
-//             }}
-//             ModalProps={{
-//               keepMounted: true, // Better open performance on mobile.
-//             }}
-//           >
-//             {drawer}
-//           </Drawer>
-//         </Hidden>
-//         <Hidden xsDown implementation="css">
-//           <Drawer
-//             classes={{
-//               paper: classes.drawerPaper,
-//             }}
-//             variant="permanent"
-//             open
-//           >
-//             {drawer}
-//           </Drawer>
-//         </Hidden>
-//       </nav>
-//       <main className={classes.content}>
-//         <div className={classes.toolbar} />
-//         <Home />
-//         {/* <Typography paragraph>
-//           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-//           eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-//           dolor purus non enim praesent elementum facilisis leo vel. Risus at
-//           ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-//           quisque non tellus. Convallis convallis tellus id interdum velit
-//           laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-//           adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-//           integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-//           eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-//           quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-//           vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-//           lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-//           faucibus et molestie ac.
-//         </Typography>
-//         <Typography paragraph>
-//           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-//           ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-//           elementum integer enim neque volutpat ac tincidunt. Ornare suspendisse
-//           sed nisi lacus sed viverra tellus. Purus sit amet volutpat consequat
-//           mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis
-//           risus sed vulputate odio. Morbi tincidunt ornare massa eget egestas
-//           purus viverra accumsan in. In hendrerit gravida rutrum quisque non
-//           tellus orci ac. Pellentesque nec nam aliquam sem et tortor. Habitant
-//           morbi tristique senectus et. Adipiscing elit duis tristique
-//           sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
-//           eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
-//           posuere sollicitudin aliquam ultrices sagittis orci a.
-//         </Typography> */}
-//       </main>
-//     </div>
-//   );
-// }
-
-// ResponsiveDrawer.propTypes = {
-//   /**
-//    * Injected by the documentation to work in an iframe.
-//    * You won't need it on your project.
-//    */
-//   window: PropTypes.func,
-// };
-
-// export default ResponsiveDrawer;
---
+++ src/Components/SongList.js
@@ -0,0 +1,104 @@
+import { Box, Card, Grid, GridListTile, Typography } from '@material-ui/core'
+import React from 'react'
+
+const SongList = () => {
+    return (
+        <Box style={{width:"772px", height:"400px",border:" 1px solid black"}}>
+            {/* <h1>
+                Song lists
+            </h1> */}
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+            <Card style={{margin:"10px",height:"45px"}}>
+                <Grid>
+                    <Grid item container style={{display:"flex",justifyContent:"space-between"}}>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                        <Typography>
+                            Hello
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Card>
+        </Box>
+    )
+}
+
+export default SongList
