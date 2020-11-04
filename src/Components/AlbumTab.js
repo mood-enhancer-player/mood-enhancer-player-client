fed5cc3d5ff4745e0450149d681d439eaf83e0c9
ShubhamKhunt <shubhamkhunt08>
2020-11-04 17:40:47 +0530

    Album tab link to server

--- src/Components/AlbumTab.js
+++ src/Components/AlbumTab.js
@@ -1,58 +1,87 @@
-import { CardContent, CardMedia, Grid, makeStyles, Paper, Typography } from '@material-ui/core'
-import React from 'react';
+import React from "react";
+import {
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Grid,
+  makeStyles,
+  Paper,
+  Typography,
+} from "@material-ui/core";
 import TabCard from "./TabCard";
+import { useQuery, gql } from "@apollo/client";
+
 const useStyles = makeStyles((theme) => ({
-    root: {
-        backgroundColor: "black",
-        display:"flex",
-        padding:"10px"
-    },
-    paper: {
-        width: "200px",
-        height: "200px",
-        margin: "20px"
-    }
+  root: {
+    backgroundColor: "black",
+    display: "flex",
+    padding: "10px",
+  },
+  paper: {
+    width: "200px",
+    height: "200px",
+    margin: "20px",
+  },
 }));
 
 const data = [
-    // {
-    //     id:1,
-    //     name:" Hasi Ban Gaye"
-    // },
-    // {
-    //     id:2,
-    //     name:" Hasi Ban Gaye"
-    // },
-    {
-        id:3,
-        name:"Rustom"
-    },
-    {
-        id:4,
-        name:"IPL"
-    },
-    {
-        id:5,
-        name:"Baghi3"
-    },
-]
+  // {
+  //     id:1,
+  //     name:" Hasi Ban Gaye"
+  // },
+  // {
+  //     id:2,
+  //     name:" Hasi Ban Gaye"
+  // },
+  {
+    id: 3,
+    name: "Rustom",
+  },
+  {
+    id: 4,
+    name: "IPL",
+  },
+  {
+    id: 5,
+    name: "Baghi3",
+  },
+];
 
 const AlbumTab = () => {
-    const classes = useStyles();
-    return (
+  const classes = useStyles();
+  const { loading, data, error } = useQuery(GET_ALBUM_QUERY);
+
+  if (data) {
+    console.log(data);
+  }
+
+  return (
+    <>
+      {loading || !data ? (
+        <CircularProgress />
+      ) : (
         <div className={classes.root}>
-            <Grid container spacing={2}>
-            {
-                data.map((text) => {
-                    return(
-                        <TabCard musicData={text} key={text.id} />
-                    )
-                })
-            }
-            </Grid>
-
+          <Grid container spacing={2}>
+            {data.getAlbums.map(({ _id, album, cover }) => {
+              return (
+                <TabCard name={album} picture={cover} id={_id} key={_id} />
+              );
+            })}
+          </Grid>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default AlbumTab
+const GET_ALBUM_QUERY = gql`
+  query {
+    getAlbums {
+      _id
+      album
+      cover
+    }
+  }
+`;
+
+export default AlbumTab;
