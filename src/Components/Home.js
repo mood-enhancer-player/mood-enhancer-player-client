094e11b9090de378a2faf3b9714d920bdcba293d
ShubhamKhunt <shubhamkhunt08>
2020-11-05 17:27:16 +0530

    update song id

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -129,7 +129,7 @@ const audioLists = [
 function Home() {
   const classes = useStyles();
   const musicInfo = useQuery(MUSIC_INFO_QUERY);
-  const [songIdState, setSongIdState] = useState("5fa384550d6d71175002ef71");
+  const [songIdState, setSongIdState] = useState("5fa3e775f2b4bb3c08e79a26");
 
   const getSongById = useQuery(GET_SONG_BY_ID_QUERY, {
     variables: {
