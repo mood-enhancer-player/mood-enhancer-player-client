5b04df59e845e69ca02fa670a63a4ccbaaf0655e
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-11-20 08:28:16 +0530

    Privacy & Policy component design done

--- src/Components/Common/Privacy.js
+++ src/Components/Common/Privacy.js
@@ -1,17 +1,39 @@
-import { Paper, Container } from "@material-ui/core";
+import {
+  Paper,
+  Container,
+  Typography,
+  makeStyles,
+  Divider,
+} from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles({
+  title: {
+    textAlign: "center",
+    marginBottom: "10px",
+  },
+  text: {
+    marginTop: "20px",
+    // marginBottom: "20px",
+    textAlign: "justify",
+  },
+});
+
 const Privacy = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <h1 style={{ textAlign: "center", marginBottom: "10px" }}>Privacy</h1>
-      <Container style={{ textAlign: "justify" }}>
-        <p>
+      <Container>
+        <h1 className={classes.title}>Privacy & Policy</h1>
+        <Divider />
+        <p className={classes.text}>
           This Privacy Policy describes how your personal information is
           collected, used, and shared when you visit the “Site”.
         </p>
-        <h2>Personal Information We Collect</h2>
-        <p>
+        <h2 className={classes.text}>Personal Information We Collect</h2>
+        <Divider />
+        <p className={classes.text}>
           When you visit the Site, we automatically collect certain information
           about your device, including information about your web browser, IP
           address, time zone, and some of the cookies that are installed on your
@@ -21,9 +43,11 @@ const Privacy = () => {
           about how you interact with the Site. We refer to this
           automatically-collected information as “Device Information.”
         </p>
-        <p>We collect Device Information using the following technologies:</p>
+        <p className={classes.text}>
+          We collect Device Information using the following technologies:
+        </p>
         <ul>
-          <li>
+          <li className={classes.text}>
             “Cookies” are data files that are placed on your device or computer
             and often include an anonymous unique identifier. For more
             information about cookies, and how to disable cookies, visit{" "}
@@ -33,30 +57,34 @@ const Privacy = () => {
             .
           </li>
 
-          <li>
+          <li className={classes.text}>
             “Log files” track actions occurring on the Site, and collect data
             including your IP address, browser type, Internet service provider,
             referring/exit pages, and date/time stamps.
           </li>
-          <li>
+          <li className={classes.text}>
             “Web beacons,” “tags,” and “pixels” are electronic files used to
             record information about how you browse the Site.
           </li>
         </ul>
-        <p>
+        <p className={classes.text}>
           When we talk about “Personal Information” in this Privacy Policy, we
           are talking both about Device Information and Order Information.
         </p>
-        <h2>How do we use your personal information?</h2>
-        <p>
+        <h2 className={classes.text}>
+          How do we use your personal information?
+        </h2>
+        <Divider />
+        <p className={classes.text}>
           We use the Device Information that we collect to help us screen for
           potential risk and fraud (in particular, your IP address), and more
           generally to improve and optimize our Site (for example, by generating
           analytics about how our customers browse and interact with the Site,
           and to assess the success of our marketing and advertising campaigns).
         </p>
-        <h2>Sharing your personal information</h2>
-        <p>
+        <h2 className={classes.text}>Sharing your personal information</h2>
+        <Divider />
+        <p className={classes.text}>
           We share your Personal Information with third parties to help us use
           your Personal Information, as described above. For example, we use
           Google Analytics to help us understand how our customers use the
@@ -74,8 +102,9 @@ const Privacy = () => {
           </a>
           .
         </p>
-        <h2>Behavioural Advertising</h2>
-        <p>
+        <h2 className={classes.text}>Behavioural Advertising</h2>
+        <Divider />
+        <p className={classes.text}>
           As described above, we use your Personal Information to provide you
           with targeted advertisements or marketing communications we believe
           may be of interest to you. For more information about how targeted
@@ -89,7 +118,7 @@ const Privacy = () => {
           </a>
           .
         </p>
-        <p>
+        <p className={classes.text}>
           You can opt out of targeted advertising by: <br />
           GOOGLE &#8211;{" "}
           <a
@@ -99,7 +128,7 @@ const Privacy = () => {
             https://www.google.com/settings/ads/anonymous
           </a>
         </p>
-        <p>
+        <p className={classes.text}>
           Additionally, you can opt out of some of these services by visiting
           the Digital Advertising Alliance’s opt-out portal at:{" "}
           <a href="http://optout.aboutads.info" target="_blank">
@@ -107,21 +136,26 @@ const Privacy = () => {
           </a>
           .
         </p>
-        <h2>Do not track</h2>
-        <p>
+        <h2 className={classes.text}>Do not track</h2>
+        <Divider />
+        <p className={classes.text}>
           Please note that we do not alter our Site’s data collection and use
           practices when we see a Do Not Track signal from your browser.
         </p>
-        <h2>Changes</h2>
-        <p>
+        <h2 className={classes.text}>Changes</h2>
+        <Divider />
+        <p className={classes.text}>
           We may update this privacy policy from time to time in order to
           reflect, for example, changes to our practices or for other
           operational, legal or regulatory reasons.
         </p>
-        <h2>Contact Us</h2>
-        For more information about our privacy practices, if you have questions,
-        or if you would like to make a complaint, please contact us by e-mail at
-        shubhamkhunt08@gmail.com
+        <h2 className={classes.text}>Contact Us</h2>
+        <Divider />
+        <p className={classes.text}>
+          For more information about our privacy practices, if you have
+          questions, or if you would like to make a complaint, please contact us
+          by e-mail at shubhamkhunt08@gmail.com
+        </p>
       </Container>
     </div>
   );
