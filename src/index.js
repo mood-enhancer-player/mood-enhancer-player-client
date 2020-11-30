077ae315aead6252932bb5be887865bea6d07191
shubhamkhunt04 <shubhamkhunt08@gmail.com>
2020-11-30 19:15:22 +0530

    remove strick mode

--- src/index.js
+++ src/index.js
@@ -39,12 +39,11 @@ const client = new ApolloClient({
 });
 
 ReactDOM.render(
-  <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-    ,
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
 
